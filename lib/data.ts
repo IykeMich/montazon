e272f39a2b8d5d3d1a37aa763d23d7cd765cfa99
IykeMich <emperortigo001@gmail.com>
2025-01-22 15:09:40 +0100

    03-created-website-layout

---
+++ lib/data.ts
@@ -0,0 +1,34 @@
+const data = {
+    headerMenus: [
+        {
+            name: "Today's Deal", 
+            href: '/search?tag=todays-deal',
+        },
+        {
+            name: 'New Arrivals',
+            href: '/search?tag=new-arrival',
+        },
+        {
+            name: 'Featured Products',
+            href: '/search?tag=featured',
+        },        
+        {
+            name: 'Best Sellers',
+            href: '/search?tag=best-sele',
+        },        
+        {
+            name: 'Browsing History',
+            href: '/#browsing-history',
+        },   
+        {
+            name: 'Customer Service',
+            href: '/page/customer-service',
+        },             
+        {
+            name: 'About Us',
+            href: '/page/help',
+        },        
+    ]
+}
+
+export default data
